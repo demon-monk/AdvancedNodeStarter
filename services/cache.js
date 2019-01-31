f84d44f9ea7b896de5266f09468f53353baf1768
demon-monk <antdude@126.com>
2019-01-31 19:00:23 +0800

    ✨ Feat: implement cache logic in mongoose.Query.prototype.exec

--- services/cache.js
+++ services/cache.js
@@ -1,9 +1,24 @@
-const mongoose = require('mongoose')
-const originExec = mongoose.Query.prototype.exec
+const mongoose = require("mongoose");
+const redis = require("redis");
+const util = require("util");
 
-mongoose.Query.prototype.exec = function () {
-    console.log('to run a query')
-    const redisKey = {...this.getQuery(), ...{collection: this.mongooseCollection.name}}
-    console.log(redisKey)
-    return originExec.apply(this, arguments)
-}
+const redisUrl = "redis://localhost:6379";
+const client = redis.createClient(redisUrl);
+client.get = util.promisify(client.get);
+
+const originExec = mongoose.Query.prototype.exec;
+
+mongoose.Query.prototype.exec = async function() {
+  const redisKey = JSON.stringify({
+    ...this.getQuery(),
+    ...{ collection: this.mongooseCollection.name }
+  });
+  const cacheValue = await client.get(redisKey);
+  if (cacheValue) {
+    console.log("FROM REDIS");
+    return JSON.stringify(cacheValue);
+  }
+  const result = originExec.apply(this, arguments);
+  client.set(redisKey, JSON.stringify(result));
+  return result;
+};
