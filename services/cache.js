6587c506a5c38f9360285acf5fe4c30571c79c6e
demon-monk <antdude@126.com>
2019-01-31 18:40:22 +0800

    ✨ Feat: generate unique key for caching redis with query option and collection name

--- services/cache.js
+++ services/cache.js
@@ -3,5 +3,7 @@ const originExec = mongoose.Query.prototype.exec
 
 mongoose.Query.prototype.exec = function () {
     console.log('to run a query')
+    const redisKey = {...this.getQuery(), ...{collection: this.mongooseCollection.name}}
+    console.log(redisKey)
     return originExec.apply(this, arguments)
 }
