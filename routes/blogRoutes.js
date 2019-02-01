3ba4d1f98a7764bf17177595ff67dc5e54a58d54
demon-monk <antdude@126.com>
2019-02-01 13:30:52 +0800

    ✨ Feat: clear redis cache with middleware in post route

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const requireLogin = require('../middlewares/requireLogin');
-const {clearCache} = require('../services/cache')
+const cleanCache = require('../middlewares/cleanCache')
 
 const Blog = mongoose.model('Blog');
 
@@ -21,7 +21,7 @@ module.exports = app => {
     res.send(blogs);
   });
 
-  app.post('/api/blogs', requireLogin, async (req, res) => {
+  app.post('/api/blogs', requireLogin, cleanCache, async (req, res) => {
     const { title, content } = req.body;
 
     const blog = new Blog({
@@ -32,7 +32,6 @@ module.exports = app => {
 
     try {
       await blog.save();
-      clearCache(req.user.id)
       res.send(blog);
     } catch (err) {
       res.send(400, err);
