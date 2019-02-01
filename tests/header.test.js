351b627f097c57e8722ee96b0065ff96e8e294cb
demon-monk <antdude@126.com>
2019-02-01 14:36:54 +0800

    🔨 Refactor: move open browswer page and navigation opertions to beforeEach

--- tests/header.test.js
+++ tests/header.test.js
@@ -1,11 +1,16 @@
 const puppeteer = require("puppeteer");
 
-test("launch a browser", async () => {
-  const browser = await puppeteer.launch({
+let browser, page;
+
+beforeEach(async () => {
+  browser = await puppeteer.launch({
     headless: false
   });
-  const page = await browser.newPage();
+  page = await browser.newPage();
   await page.goto("http://localhost:3000");
-  const text  = await page.$eval('a.brand-logo', el => el.innerHTML)
-  expect(text).toEqual('Blogster')
+});
+
+test("launch a browser", async () => {
+  const text = await page.$eval("a.brand-logo", el => el.innerHTML);
+  expect(text).toEqual("Blogster");
 });
