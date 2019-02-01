1543f9833b1f4ba9e5e3aa4c01d71e3555e7dad8
demon-monk <antdude@126.com>
2019-02-01 14:59:22 +0800

    ✨ Feat: test if run into google oauth flow by clicking login button and retriving url

--- tests/header.test.js
+++ tests/header.test.js
@@ -1,4 +1,4 @@
-const puppeteer = require("puppeteer");
+const puppeteer = require("puppeteer")
 
 let browser, page;
 
@@ -11,10 +11,17 @@ beforeEach(async () => {
 });
 
 afterEach(async () => {
-    await browser.close()
-})
+  await browser.close();
+});
 
 test("the header should has the correct logo test", async () => {
   const text = await page.$eval("a.brand-logo", el => el.innerHTML);
   expect(text).toEqual("Blogster");
 });
+
+test("should goto oAuth flow when click the login button", async () => {
+  await page.click(".right a");
+  const url = await page.url();
+  expect(url).toMatch(/accounts\.google\.com/);
+});
+
