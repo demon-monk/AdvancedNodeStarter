bba8027f8eb42160d4a94a45242b5085b519d2cb
demon-monk <antdude@126.com>
2019-02-02 20:43:03 +0800

    ✨ Feat: add page helper to extract login logic to a function in Page instance with Proxy

--- tests/header.test.js
+++ tests/header.test.js
@@ -1,21 +1,16 @@
-const puppeteer = require("puppeteer");
-const sessionFactory = require("./factories/sessionFactory");
-const userFactory = require("./factories/userFactory");
-
+const Page = require("./helpers/page");
 const HOME_URL = "http://localhost:3000";
 
-let browser, page;
+
+let page;
 
 beforeEach(async () => {
-  browser = await puppeteer.launch({
-    headless: false
-  });
-  page = await browser.newPage();
-  await page.goto(HOME_URL);
+  page = await Page.build();
+  await page.goto(page.HOME_URL);
 });
 
 afterEach(async () => {
-  await browser.close();
+  await page.close();
 });
 
 test("the header should has the correct logo test", async () => {
@@ -30,12 +25,7 @@ test("should goto oAuth flow when click the login button", async () => {
 });
 
 test.only("should show logout button when use sign in", async () => {
-  const user = await userFactory();
-  const { session, sig } = sessionFactory(user);
-  await page.setCookie({ name: "session", value: session });
-  await page.setCookie({ name: "session.sig", value: sig });
-  await page.goto(HOME_URL);
-  await page.waitFor('a[href="/auth/logout"]');
+  await page.login()
   const text = await page.$eval('a[href="/auth/logout"]', el => el.innerHTML);
   expect(text).toEqual("Logout");
 });
---
+++ tests/helpers/page.js
@@ -0,0 +1,38 @@
+const puppeteer = require("puppeteer");
+const userFactory = require("../factories/userFactory");
+const sessionFactory = require("../factories/sessionFactory");
+class Page {
+  get HOME_URL() {
+    return "http://localhost:3000";
+  }
+  static async build() {
+    const browser = await puppeteer.launch({
+      headless: false
+    });
+    const originPage = await browser.newPage();
+    const customePage = new Page(originPage);
+    return new Proxy(customePage, {
+      get(target, property) {
+        return (
+          // broswer should be primier to originPage to use its close method
+          customePage[property] || browser[property] || originPage[property]
+        );
+      }
+    });
+  }
+
+  constructor(page, browser) {
+    this.page = page;
+  }
+
+  async login() {
+    const user = await userFactory();
+    const { session, sig } = sessionFactory(user);
+    await this.page.setCookie({ name: "session", value: session });
+    await this.page.setCookie({ name: "session.sig", value: sig });
+    await this.page.goto(this.HOME_URL);
+    await this.page.waitFor('a[href="/auth/logout"]');
+  }
+}
+
+module.exports = Page;
