b9fd73f5ed0b291d8e81eb34633a6ef77970944a
demon-monk <antdude@126.com>
2019-02-02 21:41:37 +0800

    ✨ Feat: organize test cases in the same condition together with describe

--- tests/blogs.test.js
+++ tests/blogs.test.js
@@ -10,9 +10,28 @@ afterEach(async () => {
   await page.close();
 });
 
-test("should show the add post button when user is logged in", async () => {
-  await page.login();
-  await page.click("a.btn-floating");
-  const text = await page.getContent("form label");
-  expect(text).toEqual("Blog Title");
+describe("when logged in and click new post btn", () => {
+  beforeEach(async () => {
+    await page.login();
+    await page.click("a.btn-floating");
+  });
+
+  test("should show the new post form", async () => {
+    const text = await page.getContent("form label");
+    expect(text).toEqual("Blog Title");
+  });
+
+  describe("when enter invalid inputs", () => {
+    beforeEach(async () => {
+      await page.click("form button");
+    });
+
+    test("should see incorrect input messages", async () => {
+      const msgExpect = "You must provide a value";
+      const titleMsg = await page.getContent(".title .red-text");
+      expect(titleMsg).toEqual(msgExpect);
+      const contentMsg = await page.getContent(".content .red-text");
+      expect(contentMsg).toEqual(msgExpect);
+    });
+  });
 });
